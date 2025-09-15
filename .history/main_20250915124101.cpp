c04f1d5584346f305daad304048fbfe40953098f
mairasafdar <mairasafdarc@gmail.com>
2025-09-15 12:48:51 +0100

    first commit

---
+++ .history/main_20250915124101.cpp
@@ -0,0 +1,6 @@
+#include <iostream>   // lets you print to the screen
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;  
+    return 0;  // tells the computer "I finished successfully"
+}
---
+++ .history/main_20250915124203.cpp
@@ -0,0 +1,5 @@
+#include <iostream>   // lets you print to the screen
+
+int main() {
+   
+}
